ac67af4a93e30affdd1f73589cdc6109570f0382
Salakh Salamov <mokhmadsalakh.salamov@gmail.com>
2024-12-02 16:27:01 +0300

    create custom hook useFetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { FethcExample } from './components/fetchExpample';
 
 function App() {
   return (
     <div className="App">
-      
+      <FethcExample />
     </div>
   );
 }
