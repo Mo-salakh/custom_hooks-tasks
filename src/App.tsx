c8d3064d1c5767de98bdcd9dd9775b6f80ff6186
Salakh Salamov <mokhmadsalakh.salamov@gmail.com>
2024-12-09 17:58:47 +0300

    complete useViewportSize custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 
-import { useHover } from './components/useHover';
+import { useHover } from './custom_hooks/useHover';
 import { FethcExample } from './components/fetchExpample';
 import { ExampleLocalStorage } from './components/ExampleLocalStorage';
+import { useViewportSize } from './custom_hooks/useViewportSize';
 
 
 function App() {
 
   const { ref, hovered } = useHover()
+  const {width, height} = useViewportSize()
 
   return (
     <div className="App">
       <div ref={ref}>
         {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
       </div>
+      <>
+        Width: { width }, height: { height }
+      </>
       <FethcExample />
       <ExampleLocalStorage />
     </div>
